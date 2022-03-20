c2f1e2ac10836580faebaef60f61c8cfea9cd48c
Marichelle Bisuna <marichellebisuna@yahoo.com>
2022-03-21 10:50:08 +1100

    Banner area fixed

--- components/Home/BannerArea.js
+++ components/Home/BannerArea.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const BannerArea = () => {
   return (
@@ -8,36 +9,42 @@ const BannerArea = () => {
         <div className="row">
             <div className="col-lg-4 col-md-6 col-12">
                 <div className="banner-wrap mb-30" data-aos="fade-up" data-aos-delay="200">
-                    <a href="product-details.html"><Image src="/assets/images/banner/banner-1.png" alt="" width={270} height={300}/></a>
+                <Link href='/products'><a><Image src="/assets/images/banner/banner-1.png" alt="" width={270} height={300}/></a></Link>
                     <div className="banner-content-1">
                         <h5>new arrival</h5>
                         <h3>Office Chair</h3>
                         <div className="banner-btn">
-                            <a href="product-details.html">Shop Now</a>
+                        <Link href='/products'>
+                        <a>Shop Now</a>
+                        </Link>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="col-lg-4 col-md-6 col-12">
                 <div className="banner-wrap mb-30" data-aos="fade-up" data-aos-delay="400">
-                    <a href="product-details.html"><Image src="/assets/images/banner/banner-2.png" alt="" width={270} height={300}/></a>
+                <Link href='/products'><a><Image src="/assets/images/banner/banner-2.png" alt="" width={270} height={300}/></a></Link>
                     <div className="banner-content-1">
                         <h5>new arrival</h5>
                         <h3>Hanging Chair</h3>
-                        <div className="banner-btn">
-                            <a href="product-details.html">Shop Now</a>
+                        <div className="banner-btn">                            
+                        <Link href='/products'>
+                        <a>Shop Now</a>
+                        </Link>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="col-lg-4 col-md-6 col-12">
                 <div className="banner-wrap mb-30" data-aos="fade-up" data-aos-delay="600">
-                    <a href="product-details.html"><Image src="/assets/images/banner/banner-3.png" alt="" width={270} height={300}/></a>
+                <Link href='/products'><a><Image src="/assets/images/banner/banner-3.png" alt="" width={270} height={300}/></a></Link>
                     <div className="banner-content-1">
                         <h5>new arrival</h5>
                         <h3>Folding Chair</h3>
                         <div className="banner-btn">
-                            <a href="product-details.html">Shop Now</a>
+                        <Link href='/products'>
+                        <a>Shop Now</a>
+                        </Link>
                         </div>
                     </div>
                 </div>
