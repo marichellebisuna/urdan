90acd51b3eb3315bac5254dfb23e0d159e6643f7
Marichelle Bisuna <marichellebisuna@yahoo.com>
2022-05-10 13:37:49 +1000

    PopUp Modal done for RelateProducts

--- components/ProductDetails/RelatedProducts.js
+++ components/ProductDetails/RelatedProducts.js
@@ -11,7 +11,17 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const RelatedProducts = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [popUpContent, setPopUpContent] = useState([]);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const showModal = (product) =>{    
+    console.log(product.title);
+    console.log(product.price);       
+    setPopUpContent([product])  
+    setShow(true)
+} 
+  
+const [isOpen, setIsOpen] = useState(false);
 const { products} = useSelector(state => state.allProducts);  
     
 const [productItems, setProductItems] = useState(products)
@@ -52,10 +62,10 @@ setProductItems(products)
                               </div>
                               <div className="product-action-wrap">
                                   <button className="product-action-btn-1" title="Wishlist"><i className="pe-7s-like"></i></button>
-                                  <button className="product-action-btn-1" title="Quick View" onClick={() => setIsOpen(true)}>
+                                  <button className="product-action-btn-1" title="Quick View" onClick={()=>showModal( product ) }> 
                                       <i className="pe-7s-look"></i>
                                   </button>
-                                  {isOpen && <Modal setIsOpen={setIsOpen} />}
+                                  
                               </div>
                               <div className="product-action-2-wrap">
                                   <button className="product-action-btn-2" title="Add To Cart"><i className="pe-7s-cart"></i> Add to cart</button>
@@ -74,7 +84,74 @@ setProductItems(products)
               </div>
           </div>
        
-          
+          {show && 
+                <div className="quickview-modal-style" onClick={handleClose}>
+                <div className="modal-dialog modal-dialog-centered" role="document" >
+                    <div className="modal-content" >
+                        <div className="modal-header">
+                            <a href="#" className="close" onClick={handleClose}><i className=" ti-close "></i></a>
+                        </div>
+                        {popUpContent.map((pop)=>{  
+                            return ( 
+                        <div className="modal-body">
+                            <div className="row gx-0">
+                                <div className="col-lg-5 col-md-5 col-12">
+                                    <div className="modal-img-wrap">                                
+                                        <Image 
+                                    src={pop.images[0].url}
+                                    alt="" width={429} height={476}/>
+                                    </div>
+                                </div>
+                                <div className="col-lg-7 col-md-7 col-12">
+                                    <div className="product-details-content quickview-content">
+                                        <h2>{pop.title}</h2>
+                                        <div className="product-details-price">
+                                            <span className="old-price">$25.89 </span>
+                                            <span className="new-price">${pop.price}</span>
+                                        </div>
+                                        <div className="product-details-review">
+                                            <div className="product-rating">
+                                                <i className=" ti-star"></i>
+                                                <i className=" ti-star"></i>
+                                                <i className=" ti-star"></i>
+                                                <i className=" ti-star"></i>
+                                                <i className=" ti-star"></i>
+                                            </div>
+                                            <span>( 1 Customer Review )</span>
+                                        </div>
+                                        <div className="product-color product-color-active product-details-color">
+                                            <span>Color :</span>
+                                            <ul>
+                                                <li><a title="Pink" className="pink" href="#">pink</a></li>
+                                                <li><a title="Yellow" className="active yellow" href="#">yellow</a></li>
+                                                <li><a title="Purple" className="purple" href="#">purple</a></li>
+                                            </ul>
+                                        </div>
+                                        <p>{pop.description}</p>
+                                        <div className="product-details-action-wrap">
+                                            <div className="product-quality">
+                                                <input className="cart-plus-minus-box input-text qty text" name="qtybutton" value="1"/>
+                                            </div>
+                                            <div className="single-product-cart btn-hover">
+                                                <a href="#">Add to cart</a>
+                                            </div>
+                                            <div className="single-product-wishlist">
+                                                <a title="Wishlist" href="#"><i className="pe-7s-like"></i></a>
+                                            </div>
+                                            <div className="single-product-compare">
+                                                <a title="Compare" href="#"><i className="pe-7s-shuffle"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        ) 
+                        })}
+                    </div>
+                </div>
+                </div>    
+            }  
       </div>
     </div>
   )
